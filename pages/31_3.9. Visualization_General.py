bd6f981e872a67e471b216530affad6436ca32e3
nomura <pi.higher@gmail.com>
2024-04-14 18:11:05 +0900

    updated

--- pages/31_3.9. Visualization_General.py
+++ pages/31_3.9. Visualization_General.py
@@ -1,28 +1,80 @@
 from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
+import numpy as np
 import pandas as pd
+import os
 import streamlit as st
- 
-# Streamlitページの幅を調整する
-st.set_page_config(
-    page_title="StreamlitでPygwalkerを使う",
-    layout="wide"
-)
- 
-# PyGWalkerとStreamlitの通信を確立する
-init_streamlit_comm()
- 
-# タイトルを追加
-st.title("StreamlitでPygwalkerを使う")
- 
-# PyGWalkerのレンダラーのインスタンスを取得する。このインスタンスをキャッシュすることで、プロセス内メモリの増加を効果的に防ぐことができます。
-@st.cache_resource
-def get_pyg_renderer() -> "StreamlitRenderer":
-    df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
-    # アプリをパブリックに公開する場合、他のユーザーがチャートの設定ファイルに書き込めないように、デバッグパラメータをFalseに設定する必要があります。
-    return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
- 
-renderer = get_pyg_renderer()
- 
-# データ探索インターフェースをレンダリングする。開発者はこれを使用してドラッグアンドドロップでチャートを作成できます。
-renderer.render_explore()
- 
+from glob import glob
+
+# def load_data(file_path):
+#     """Load data from a given CSV file path."""
+#     if file_path:
+#         return pd.read_csv(file_path)
+#     return None
+
+# データのロードと前処理
+def load_data(file_path, reduce_data=False):
+    """
+    データのロードと前処理
+    """
+    try:
+        # 日付時刻形式に変換を試みる
+        df = pd.read_csv(file_path, header=0, parse_dates=[0], index_col=0)
+    except (ValueError, TypeError):
+        # 変換が失敗した場合は通常のインデックスとして読み込む
+        df = pd.read_csv(file_path, header=0, index_col=0)
+    
+    if reduce_data:
+        # データが多い場合は間引いて表示
+        num_rows = len(df)
+        interval = max(1, np.floor(num_rows / 100).astype(int))
+        df = df.iloc[::interval, :]
+    return df
+
+def setup_streamlit(reduce_data=False):
+    """Configure the Streamlit page and sidebar for file selection."""
+    # Adjust the width of the Streamlit page
+    st.set_page_config(page_title="Data Analysis with PyGWalker", layout="wide")
+    
+    # Establish communication between PyGWalker and Streamlit
+    init_streamlit_comm()
+    
+    # Display the title
+    st.title("Exploaring Data Analysis with PyGWalker", anchor="top")
+    
+    # File selection in the sidebar
+    operating_data_directory = "./data/operating_data/"
+    operating_files = glob(os.path.join(operating_data_directory, '*.csv'))
+    
+    with st.sidebar:
+        st.sidebar.title("Select File")
+        selected_file = st.selectbox("Please select the file for analysis:", operating_files) 
+
+        st.sidebar.title("Data Reduction")
+        st.sidebar.markdown("Reduce the data to display:")
+        reduce_data = st.sidebar.checkbox("Reduce Data", value=reduce_data)
+
+        st.sidebar.button("Analyze Data", key="analyze_button")
+
+    return load_data(selected_file, reduce_data)
+
+def render_data_analysis(df):
+    """Render the data analysis interface if the dataframe is not None."""
+    if df is not None:
+        # Get the PyGWalker renderer instance. This instance is cached to effectively prevent memory increase within the process.
+        @st.cache_resource
+        def get_pyg_renderer() -> "StreamlitRenderer":
+            # Set the debug parameter to False when publishing the app publicly to prevent others from writing into the chart config file.
+            return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
+        
+        renderer = get_pyg_renderer()
+        renderer.render_explore()
+    else:
+        st.warning("Please upload a CSV file to proceed.")
+
+def main():
+    """Main function to execute the Streamlit app functionalities."""
+    df = setup_streamlit()
+    render_data_analysis(df)
+
+if __name__ == "__main__":
+    main()
