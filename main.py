1b3b58efc984b7a74b04d2d479f9f955901e7144
nomura <pi.higher@gmail.com>
2024-04-23 17:53:43 +0900

    update_mail

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ st.markdown("""
     Welcome to Plant Data Analysis Studio! This application is a data analysis platform for plant engineers. You can upload data, conduct analyses, and gain insights.
 
     ### Key Features
-    - **Data Upload**: Upload CSV files and start analyzing immediately.
-    - **Data Viewer**: View uploaded data, download files, and delete data.
-    - **Interactive Data Analysis**: Use various tools to visualize and analyze data.
-    - **Machine Learning**: Train and test machine learning models on your data.
+    1. **Data Management**: Upload CSV files and View/Delete uploaded data.
+    2. **Preprocess**: Process uploaded data for analysis.
+    3. **Visualization**: Use various tools to visualize/analyze data.
+    4. **Machine Learning**: Build meachine learning model, and execute case study with models.
 
     ### Updates
-    - April 12, 2024: Added new visual analysis tools.
-    - April 16, 2024: Add Regression Tools.
+    - April 12, 2024: 1st Deployment of Application.
+    - April 16, 2024: Add Machine Learning (Regression) Function.
+    - April 23, 2024: Add Machine Learning (Case Study) Function.
 """)
 
 st.header("", divider="blue")
