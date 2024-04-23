2ae924e0247f2480657021212909b065979cbfb1
nomura <pi.higher@gmail.com>
2024-04-23 15:00:34 +0900

    update_files

--- pages/21_2.1. Preprocess_(Define_New_Column).py
+++ pages/21_2.1. Preprocess_(Define_New_Column).py
@@ -1,4 +1,3 @@
-from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
 import pandas as pd
 import streamlit as st
 from glob import glob
@@ -10,35 +9,27 @@ from library import functions as func
 from library import config
 config.set_page_config()
 
-# 関数の定義
 def select_file(directory):
-    """
-    ディレクトリからファイルを選択する関数
-    """
     files = glob(os.path.join(directory, '*.csv'))
     if files:
         selected_file = st.selectbox("Select a file:", files)
-        return selected_file # 選択されたファイルのパスを返す
+        return selected_file
     else:
         st.write("No files found.")
         return None
 
-
-# タイトルを追加
 st.title("Define New Column")
 st.write("This page allows you to define a new column based on a calculation formula.")
-
 st.subheader("Load: Data", divider='rainbow')
 
-# データのロードと前処理
 selected_file = select_file(var.operating_dir)
 if selected_file:
-    # ページをリロード
     df = func.load_data(selected_file, reduce_data=False)
-    st.dataframe(df)
+    if 'df' not in st.session_state:
+        st.session_state.df = df
+    st.dataframe(st.session_state.df)
 
 def add_new_column(dataframe, formula, new_column_name):
-    """ Add a new column to the DataFrame based on the given formula. """
     try:
         dataframe[new_column_name] = pd.eval(formula, local_dict=dataframe)
         st.success("New column added successfully.")
@@ -47,10 +38,8 @@ def add_new_column(dataframe, formula, new_column_name):
         st.error(f"Error in calculation: {e}")
         return dataframe
 
-
-# ユーザーインターフェースの設定
 st.subheader("Define: New Column", divider='rainbow')
-if st.session_state.df is not None and not st.session_state.df.empty:
+if 'df' in st.session_state and not st.session_state.df.empty:
     with st.form("my_form"):
         cols = st.columns([2, 1])
         with cols[0]:
@@ -58,15 +47,11 @@ if st.session_state.df is not None and not st.session_state.df.empty:
         with cols[1]:
             new_column_name = st.text_input("Enter the name for the new column:")
 
-        # フォームのサブミットボタン
         submitted = st.form_submit_button("Add New Column")
         if submitted:
             st.session_state.df = add_new_column(st.session_state.df, calc_formula, new_column_name)
             st.dataframe(st.session_state.df)
 
-
-
-# # ファイルの保存
 data_type = "Operating Data"
 save_directory = var.operating_dir
 func.save_data(st.session_state.df, data_type, save_directory)
