8359e59eb87ba7877701c83f82d445fa5cb8932f
nomura <pi.higher@gmail.com>
2024-04-23 15:09:09 +0900

    update_codes

--- pages/32_3.2. Visualization_(Dashboard).py
+++ pages/32_3.2. Visualization_(Dashboard).py
@@ -10,7 +10,6 @@ from library import functions as func
 from library import config
 config.set_page_config(layout="wide")
 
-# 関数の定義
 def select_file(directory):
     """
     ディレクトリからファイルを選択する関数
@@ -18,42 +17,33 @@ def select_file(directory):
     files = glob(os.path.join(directory, '*.csv'))
     if files:
         selected_file = st.selectbox("Select a file:", files)
-        return selected_file # 選択されたファイルのパスを返す
+        return selected_file
     else:
         st.write("No files found.")
         return None
 
-
-# タイトルを追加
 st.title("Visualization Dashboard")
 st.write("This page provides interactive visualization capabilities for data analysis.")
-
 st.subheader("Load: Data", divider='rainbow')
 
-# データのロードと前処理
 selected_file = select_file(var.operating_dir)
 if selected_file:
-    # ページをリロード
     df = func.load_data(selected_file, reduce_data=False)
-    # df = pd.read_csv(selected_file, header=0)
-
     if st.checkbox("Show data"):
         st.dataframe(df)
 
-st.subheader("Visualize:", divider='rainbow')
-
-if st.button("Open Dashboard", key="open_dashboard"):
-    # PyGWalkerとStreamlitの通信を確立する
-    init_streamlit_comm()
- 
-    # PyGWalkerのレンダラーのインスタンスを取得する。このインスタンスをキャッシュすることで、プロセス内メモリの増加を効果的に防ぐことができます。
-    @st.cache_resource
-    def get_pyg_renderer() -> "StreamlitRenderer":
-        # df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
-        # アプリをパブリックに公開する場合、他のユーザーがチャートの設定ファイルに書き込めないように、デバッグパラメータをFalseに設定する必要があります。
-        return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
-    
-    renderer = get_pyg_renderer()
-    
-    # データ探索インターフェースをレンダリングする。開発者はこれを使用してドラッグアンドドロップでチャートを作成できます。
-    renderer.render_explore()
+    st.subheader("Visualize:", divider='rainbow')
+    if st.button("Open Dashboard", key="open_dashboard"):
+        # PyGWalkerとStreamlitの通信を確立する
+        init_streamlit_comm()
+     
+        # PyGWalkerのレンダラーのインスタンスを取得する。このインスタンスをキャッシュすることで、プロセス内メモリの増加を効果的に防ぐことができます。
+        @st.cache_resource
+        def get_pyg_renderer() -> "StreamlitRenderer":
+            # データフレーム `df` をレンダラーに渡す
+            return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
+        
+        renderer = get_pyg_renderer()
+        
+        # データ探索インターフェースをレンダリングする。開発者はこれを使用してドラッグアンドドロップでチャートを作成できます。
+        renderer.render_explore()
